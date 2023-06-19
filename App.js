44a52ba71bd19d8bb74f999972560ad4b107fb1f
Yisak Girmay <yisakbruck23@gmail.com>
2023-06-19 15:04:45 -0400

    create app component

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpenseTracker from "./ExpenseTracker";
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseTracker />
+    </div>
+  );
+}
+
+export default App;
