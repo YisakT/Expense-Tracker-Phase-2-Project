95c14f8d7e29ce9ab2f153432ed1ed16b2f7b0f4
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-01 19:18:08 -0400

    Home page update

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
 
 function HomePage() {
-  return <h1>Welcome to Expense Tracker App</h1>;
+  const divStyle = {
+    backgroundColor: "#85bb65",
+    padding: "20px",
+  };
+
+  return (
+    <div style={divStyle}>
+      <h1>Welcome to Expense Tracker App</h1>
+      <img src={image1} alt="Description of image 1" style={{width: '100%', height: 'auto'}}/>
+      <img src={image2} alt="Description of image 2" style={{width: '100%', height: 'auto'}}/>
+    </div>
+  );
 }
 
 export default HomePage;
