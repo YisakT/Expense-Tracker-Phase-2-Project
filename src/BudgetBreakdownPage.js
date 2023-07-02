7faed31a825f1e23c8cf03f71813acaf37ec483b
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-02 15:41:41 -0400

    code clean up

--- src/BudgetBreakdownPage.js
+++ src/BudgetBreakdownPage.js
@@ -1,28 +1,4 @@
 
-// import React, { useContext } from "react";
-// import ExpenseSummary from "./ExpenseSummary.js";
-// import { ExpenseContext } from './ExpenseProvider';
-
-// function BudgetBreakdownPage() {
-//   const { expenses, budget } = useContext(ExpenseContext);
-  
-//   const totalExpenses = expenses.reduce((total, expense) => total + parseFloat(expense.amount), 0);
-//   const remainingBudget = budget - totalExpenses;
-
-//   return (
-//     <div>
-//       <h1>Budget Breakdown Page</h1>
-//       <ExpenseSummary
-//         totalExpenses={totalExpenses}
-//         remainingBudget={remainingBudget}
-//       />
-//     </div>
-//   );
-// }
-
-// export default BudgetBreakdownPage;
-
-
 import React, { useContext } from "react";
 import { PieChart, Pie, Cell, ResponsiveContainer } from "recharts";
 import ExpenseSummary from "./ExpenseSummary.js";
