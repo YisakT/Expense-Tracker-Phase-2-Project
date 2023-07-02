1bb3d9b64fc14b83d932235df9e925354a72c46c
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-02 16:11:15 -0400

    css update for expense tracker

--- src/ExpenseTracker.js
+++ src/ExpenseTracker.js
@@ -1,10 +1,51 @@
 
+// import React, { useContext } from "react";
+// import ExpenseForm from "./ExpenseForm";
+// import ExpenseList from "./ExpenseList";
+// import BudgetForm from "./BudgetForm";
+// import ExpenseSummary from "./ExpenseSummary.js";
+// import { ExpenseContext } from './ExpenseProvider';
+
+// function ExpenseTracker() {
+//   const { expenses, addExpense, deleteExpense, budget, updateBudget } = useContext(ExpenseContext);
+
+//   const totalExpenses = expenses.reduce(
+//     (total, expense) => total + parseFloat(expense.amount),
+//     0
+//   );
+
+//   const remainingBudget = budget - totalExpenses;
+
+
+//   return (
+//     <div>
+//       <h1>Expense Tracker</h1>
+//       <BudgetForm updateBudget={updateBudget} />
+
+//       <h2>Add Expense</h2>
+//       <ExpenseForm />
+
+//       <h2>Expenses</h2>
+//       <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
+
+//       <h2>Expense Summary</h2>
+//       <ExpenseSummary totalExpenses={totalExpenses} remainingBudget={remainingBudget} />
+
+      
+//     </div>
+//   );
+// }
+
+// export default ExpenseTracker;
+
+
 import React, { useContext } from "react";
 import ExpenseForm from "./ExpenseForm";
 import ExpenseList from "./ExpenseList";
 import BudgetForm from "./BudgetForm";
 import ExpenseSummary from "./ExpenseSummary.js";
 import { ExpenseContext } from './ExpenseProvider';
+import piggyBankImage from './images/piggy-bank-and-budget-money.jpg';
 
 function ExpenseTracker() {
   const { expenses, addExpense, deleteExpense, budget, updateBudget } = useContext(ExpenseContext);
@@ -16,24 +57,49 @@ function ExpenseTracker() {
 
   const remainingBudget = budget - totalExpenses;
 
-
   return (
-    <div>
-      <h1>Expense Tracker</h1>
+    <div style={{ backgroundImage: `url(${piggyBankImage})`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '100vh' }}>
+      <h1 style={{ color: 'white' }}>Expense Tracker</h1>
       <BudgetForm updateBudget={updateBudget} />
 
-      <h2>Add Expense</h2>
+      <h2 style={{ color: 'white' }}>Add Expense</h2>
       <ExpenseForm />
 
-      <h2>Expenses</h2>
+      <h2 style={{ color: 'white' }}>Expenses</h2>
       <ExpenseList expenses={expenses} deleteExpense={deleteExpense} />
 
-      <h2>Expense Summary</h2>
-      <ExpenseSummary totalExpenses={totalExpenses} remainingBudget={remainingBudget} />
+      <h2 style={{ color: 'white' }}>Expense Summary</h2>
+      <div style={{ color: 'white' }}>
+        <ExpenseSummary totalExpenses={totalExpenses} remainingBudget={remainingBudget} />
+      </div>
 
-      
+      <style>{`
+        button {
+          color: white;
+          background-color: orange;
+          border: none;
+          padding: 10px 20px;
+          margin: 10px 0;
+          cursor: pointer;
+        }
+
+        .total-expenses {
+          color: white;
+          font-weight: bold;
+        }
+
+        .remaining-budget {
+          color: white;
+          font-weight: bold;
+        }
+        
+        .expense-item {
+          color: white;
+        }
+      `}</style>
     </div>
   );
 }
 
 export default ExpenseTracker;
+
