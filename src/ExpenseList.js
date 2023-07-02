c5d7b7ffc9ecf2316a4844a9ae072c0c1746a732
Yisak Girmay <yisakbruck23@gmail.com>
2023-07-01 20:17:51 -0400

    fixed issue with ExpenseList

--- src/ExpenseList.js
+++ src/ExpenseList.js
@@ -1,34 +1,22 @@
-import React from "react";
 
-function ExpenseList({ expenses = [], deleteExpense }) {
+import React from 'react';
+import { ExpenseConsumer } from './ExpenseProvider';
+
+const ExpenseList = () => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Description</th>
-          <th>Amount</th>
-          <th>Classification</th>
-          <th>Receipt</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {expenses.map((expense, index) => (
-          <tr key={index}>
-            <td>{expense.description}</td>
-            <td>${expense.amount}</td>
-            <td>{expense.classification}</td>
-            <td>{expense.hasReceipt}</td>
-            <td>
-              <button onClick={() => deleteExpense(index)}>Delete</button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <ExpenseConsumer>
+      {({ expenses, deleteExpense }) => (
+        <div>
+          {expenses.map((expense, index) => (
+            <div key={expense.id}>
+              <p>{expense.description}: ${expense.amount}</p>
+              <button onClick={() => deleteExpense(expense.id)}>Delete</button>
+            </div>
+          ))}
+        </div>
+      )}
+    </ExpenseConsumer>
   );
-}
-
+};
 
 export default ExpenseList;
-
